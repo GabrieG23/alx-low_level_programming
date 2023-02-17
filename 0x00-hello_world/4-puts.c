278bf27241220a3d51708cd87c595a9c9aee3a5c
GabrieG23 <gabrielemmanuel1550@gmail.com>
2023-02-17 03:14:30 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 /* main - A Programming is like building a multilingual puzzle*/
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+	return (0); }
