20c4fb37bccce2bb5f57cc2cf3cdc938c125e5da
GabrieG23 <gabrielemmanuel1550@gmail.com>
2023-02-20 05:49:44 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main UNIX basic
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful" -Dora Korpar, 2015-10-19\n", 61);
+	return (0);
+}
